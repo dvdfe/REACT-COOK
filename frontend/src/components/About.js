802b1fb608f20666345f15c52c1de9153977d179
DVDFE <davidfe.pro@gmail.com>
2023-07-26 18:09:56 +0200

    Première ébauche da la page about

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,16 +1,29 @@
-import React from 'react';
-import Header from './Header';
-import Navigation from './Navigation';
+import React from "react";
+import Header from "./Header";
+import Navigation from "./Navigation";
 
 const About = () => {
-    return (
-        <div>
-            <Header/>
-            <Navigation/>
-            <h1>A propos</h1>
-            
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <Navigation />
+      <h2>A propos</h2>
+      <p>
+        MARMICOOK est une application web react avec appel à une API de cuisine,
+        j'utilise plusieurs technique javascript et react pour m'entraîner, le
+        site sera sûrement amener à évoluer avec le temps. Pour l'appel à l'API
+        j'utilise axios, j'utilise également sass et bootstrap-react.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit fugiat
+        nisi veritatis harum nostrum corrupti qui, quos est quisquam rem
+        officiis placeat ea minima odio dolore ad. Nisi nihil quas doloribus
+        impedit sapiente hic adipisci velit ex officiis modi nostrum sequi
+        explicabo animi earum quasi numquam, optio nemo, consequatur reiciendis.
+      </p>
+    </div>
+  );
 };
 
 export default About;
